0d1440826f73b539c6e94ce1e93a2c44947e7cf7
Patrick Reynolds <patrick.reynolds@github.com>
2019-01-25 19:58:33 -0500

    Use str.substring instead of str.substr
    str.substr's second argument is the length of the string, not the final
    index.  `str.substr(..., str.length)` ends up the same, because there are
    no index-out-of-bounds errors, but `str.substring(..., str.length)` feels
    tidier.

--- js/lexer.js
+++ js/lexer.js
@@ -6,26 +6,26 @@ function lex(str) {
 		var match
 		if (match = str.match(/^\s+/)) {
 			// console.log("whitespace: <" + match[0] + ">")
-			str = str.substr(match[0].length, str.length)
+			str = str.substring(match[0].length, str.length)
 		}
 		else if (match = str.match(/^#.*\n/)) {
 			// console.log("comment: " + match[0].trim())
-			str = str.substr(match[0].length, str.length)
+			str = str.substring(match[0].length, str.length)
 		}
 		else if (match = str.match(/^[_a-zA-Z][_0-9a-zA-Z]*/)) {
 			// console.log("bareword: " + match[0])
 			ret.push(["BAREWORD", match[0]])
-			str = str.substr(match[0].length, str.length)
+			str = str.substring(match[0].length, str.length)
 		}
 		else if (match = str.match(/^\d+/)) {
 			// console.log("integer: " + match[0])
 			ret.push(["INTEGER", parseInt(match[0], 10)])
-			str = str.substr(match[0].length, str.length)
+			str = str.substring(match[0].length, str.length)
 		}
 		else if (match = str.match(/^[={}\[\],]/)) {
 			// console.log("operator: " + match[0])
 			ret.push(["OPERATOR", match[0]])
-			str = str.substr(match[0].length, str.length)
+			str = str.substring(match[0].length, str.length)
 		}
 		else if (str.charAt(0) == '"') {
 			var val = ""
