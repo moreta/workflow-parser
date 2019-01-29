c4e505eee2c00c8fd35c154891d9bd721339874b
Patrick Reynolds <patrick.reynolds@github.com>
2019-01-29 17:42:43 -0500

    Don't hardcode foo.workflow

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ var fs = require('fs')
 var lexer = require('./lexer')
 var parser = require('./parser')
 
-var contents = fs.readFileSync("../foo.workflow", "utf8")
+if (process.argv.length != 3) {
+	console.log("Usage:")
+	console.log("  " + process.argv[1] + " filename.workflow")
+	return
+}
+var contents = fs.readFileSync(process.argv[2], "utf8")
 var tokens = lexer.lex(contents)
 var ast = parser.parseWorkflowFile(tokens, [0])
 if (!ast || (ast.length > 0 && !ast[ast.length-1])) {
