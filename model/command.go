e32b68383c2d6f3dd593ca303a46316ab4cb085f
Gudmundur Bjarni Olafsson <gudmundur.bjarni@gmail.com>
2019-01-31 10:14:21 +0100

    Add a comment on the Command type

--- model/command.go
+++ model/command.go
@@ -4,6 +4,13 @@ import (
 	"strings"
 )
 
+// Command represents the optional "runs" and "args" attributes.
+// Each one takes one of two forms:
+//   - runs="entrypoint arg1 arg2 ..."
+//   - runs=[ "entrypoint", "arg1", "arg2", ... ]
+// If the user uses the string form, the StringCommand type should be used.
+// If the user uses the list form, the ListCommand type should be used.
+// Both types have methods to either split or join depending on the use.
 type Command interface {
 	isCommand()
 	Split() []string
