2c0ff01fe0bc66d2814a9cffe33605819e2a89c2
Jonathan Clem <jclem@github.com>
2019-02-02 10:04:25 -0500

    Convert indentation to spaces

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ var lexer = require('./lexer')
 var parser = require('./parser')
 
 if (process.argv.length != 3) {
-	console.log("Usage:")
-	console.log("  " + process.argv[1] + " filename.workflow")
-	return
+  console.log("Usage:")
+  console.log("  " + process.argv[1] + " filename.workflow")
+  return
 }
 var contents = fs.readFileSync(process.argv[2], "utf8")
 var tokens = lexer.lex(contents)
 var ast = parser.parseWorkflowFile(tokens, [0])
 if (!ast || (ast.length > 0 && !ast[ast.length-1])) {
-	return
+  return
 }
 
 console.log(JSON.stringify(ast, null, 2))
