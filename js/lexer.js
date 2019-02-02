7ad66061bd2a1ee5e6c30d24972767363daf0ef7
Jonathan Clem <jclem@github.com>
2019-02-02 10:23:38 -0500

    Add passing ESLint

--- js/lexer.js
+++ js/lexer.js
@@ -1,109 +1,135 @@
-exports.lex = lex
+exports.lex = lex;
 
 function countNewlines(str, start, end) {
-  var ret = 0
-  for (var i=start; i<end; i++)
-    if (str.charAt(i) == '\n') ret++
-  return ret
+  let ret = 0;
+  for (let i = start; i < end; i++) if (str.charAt(i) === "\n") ret++;
+  return ret;
 }
 
 function lex(str) {
-  var ret = []
-  var linenum = 1
-  while (str != "") {
-    var match
+  const ret = [];
+  let linenum = 1;
+  while (str !== "") {
+    let match;
     // Skip whitespace
-    if (match = str.match(/^[ \t\n\r]/)) {
+    if ((match = str.match(/^[ \t\n\r]/))) {
       // console.log("whitespace: <" + match[0] + ">")
-      linenum += countNewlines(str, 0, match[0].length)
-      str = str.substring(match[0].length, str.length)
+      linenum += countNewlines(str, 0, match[0].length);
+      str = str.substring(match[0].length, str.length);
     }
 
     // Skip comments
-    else if (match = str.match(/^(?:#|\/\/).*/)) {
+    else if ((match = str.match(/^(?:#|\/\/).*/))) {
       // console.log("comment: " + match[0].trim())
-      linenum += countNewlines(str, 0, match[0].length)
-      str = str.substring(match[0].length, str.length)
+      linenum += countNewlines(str, 0, match[0].length);
+      str = str.substring(match[0].length, str.length);
     }
 
     // Barewords
-    else if (match = str.match(/^[_a-zA-Z][_0-9a-zA-Z]*/)) {
+    else if ((match = str.match(/^[_a-zA-Z][_0-9a-zA-Z]*/))) {
       // console.log("bareword: " + match[0])
-      ret.push(["BAREWORD", match[0], linenum])
-      str = str.substring(match[0].length, str.length)
+      ret.push(["BAREWORD", match[0], linenum]);
+      str = str.substring(match[0].length, str.length);
     }
 
     // Integers
-    else if (match = str.match(/^\d+/)) {
+    else if ((match = str.match(/^\d+/))) {
       // console.log("integer: " + match[0])
-      ret.push(["INTEGER", parseInt(match[0], 10), linenum])
-      str = str.substring(match[0].length, str.length)
+      ret.push(["INTEGER", parseInt(match[0], 10), linenum]);
+      str = str.substring(match[0].length, str.length);
     }
 
     // Single-character operators
-    else if (match = str.match(/^[={}\[\],]/)) {
+    else if ((match = str.match(/^[={}[],]/))) {
       // console.log("operator: " + match[0])
-      ret.push(["OPERATOR", match[0], linenum])
-      str = str.substring(match[0].length, str.length)
+      ret.push(["OPERATOR", match[0], linenum]);
+      str = str.substring(match[0].length, str.length);
     }
 
     // Strings, with escape characters
-    else if (str.charAt(0) == '"') {
-      var val = ""
-      var warnedAboutIllegalChar = false
-      for (var i=1; i<=str.length; i++) {
+    else if (str.charAt(0) === '"') {
+      let val = "";
+      let warnedAboutIllegalChar = false;
+      for (let i = 1; i <= str.length; i++) {
         if (i >= str.length) {
-          ret.push(["ERROR", "unterminated string literal", linenum])
-          return ret
+          ret.push(["ERROR", "unterminated string literal", linenum]);
+          return ret;
         }
-        if (str.charAt(i) == '"') {
-          str = str.substring(i+1, str.length)
-          break
+        if (str.charAt(i) === '"') {
+          str = str.substring(i + 1, str.length);
+          break;
         }
-        if (str.charAt(i) == '\\') {
+        if (str.charAt(i) === "\\") {
           switch (str.charAt(++i)) {
-            case 'b':   val += '\b';  break
-            case 'f':   val += '\f';  break
-            case 'n':   val += '\n';  break
-            case 'r':   val += '\r';  break
-            case 't':   val += '\t';  break
-            case '"':   val += '"';   break
-            case '\\':  val += '\\';  break
+            case "b":
+              val += "\b";
+              break;
+            case "f":
+              val += "\f";
+              break;
+            case "n":
+              val += "\n";
+              break;
+            case "r":
+              val += "\r";
+              break;
+            case "t":
+              val += "\t";
+              break;
+            case '"':
+              val += '"';
+              break;
+            case "\\":
+              val += "\\";
+              break;
             default:
-              ret.push(["ERROR", "illegal escape sequence: \"" + str.charAt(i) + "\"", linenum])
+              ret.push([
+                "ERROR",
+                `illegal escape sequence: "${str.charAt(i)}"`,
+                linenum
+              ]);
           }
-        }
-        else if (str.charCodeAt(i) < 32) {
+        } else if (str.charCodeAt(i) < 32) {
           if (!warnedAboutIllegalChar) {
-            ret.push(["ERROR", "control character in string: '\\u00" + str.charCodeAt(i).toString(16).padStart(2, '0') + "'", linenum])
-            warnedAboutIllegalChar = true
+            ret.push([
+              "ERROR",
+              `control character in string: '\\u00${str
+                .charCodeAt(i)
+                .toString(16)
+                .padStart(2, "0")}'`,
+              linenum
+            ]);
+            warnedAboutIllegalChar = true;
           }
-        }
-        else {
-          val += str.charAt(i)
+        } else {
+          val += str.charAt(i);
         }
       }
-      ret.push(["STRING", val, linenum])
+      ret.push(["STRING", val, linenum]);
     }
 
     // Heredocs
-    else if (match = str.match(/^<<([_a-zA-Z][_0-9a-zA-Z]*)/)) {
-      var terminator = match[1]
-      var endpos = str.indexOf(terminator, match[0].length)
-      if (endpos == -1) {
-        ret.push(["ERROR", "missing heredoc terminator: \"" + terminator + "\"", linenum])
-        return ret
+    else if ((match = str.match(/^<<([_a-zA-Z][_0-9a-zA-Z]*)/))) {
+      const terminator = match[1];
+      const endpos = str.indexOf(terminator, match[0].length);
+      if (endpos === -1) {
+        ret.push([
+          "ERROR",
+          `missing heredoc terminator: "${terminator}"`,
+          linenum
+        ]);
+        return ret;
       }
-      ret.push(["STRING", str.substring(match[0].length, endpos), linenum])
-      linenum += countNewlines(str, 0, endpos+terminator.length)
-      str = str.substring(endpos + terminator.length, str.length)
+      ret.push(["STRING", str.substring(match[0].length, endpos), linenum]);
+      linenum += countNewlines(str, 0, endpos + terminator.length);
+      str = str.substring(endpos + terminator.length, str.length);
     }
 
     // Else fail
     else {
-      ret.push(["ERROR", "illegal character: \"" + str.charAt(0) + "\"", linenum])
-      return ret
+      ret.push(["ERROR", `illegal character: "${str.charAt(0)}"`, linenum]);
+      return ret;
     }
   }
-  return ret
+  return ret;
 }
--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-var fs = require('fs')
-var lexer = require('./lexer')
-var parser = require('./parser')
+const fs = require("fs");
+const lexer = require("./lexer");
+const parser = require("./parser");
 
-if (process.argv.length != 3) {
-  console.log("Usage:")
-  console.log("  " + process.argv[1] + " filename.workflow")
-  return
-}
-var contents = fs.readFileSync(process.argv[2], "utf8")
-var tokens = lexer.lex(contents)
-var ast = parser.parseWorkflowFile(tokens, [0])
-if (!ast || (ast.length > 0 && !ast[ast.length-1])) {
-  return
-}
+main();
 
-console.log(JSON.stringify(ast, null, 2))
+function main() {
+  if (process.argv.length !== 3) {
+    console.log("Usage:");
+    console.log(`  ${process.argv[1]} filename.workflow`);
+    return;
+  }
+  const contents = fs.readFileSync(process.argv[2], "utf8");
+  const tokens = lexer.lex(contents);
+  const ast = parser.parseWorkflowFile(tokens, [0]);
+  if (!ast || (ast.length > 0 && !ast[ast.length - 1])) {
+    return;
+  }
+
+  console.log(JSON.stringify(ast, null, 2));
+}
